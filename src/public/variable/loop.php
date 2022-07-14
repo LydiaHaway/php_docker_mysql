ac8635717891ac8c4882936bfe1f22f5e0c41f44
Lydia <lydia.haway@gmail.com>
2022-07-14 09:52:28 +0200

    ex

--- src/public/variable/loop.php
+++ src/public/variable/loop.php
@@ -1,6 +1,20 @@
 <?php
 
 $pronouns = array ('I', 'You', 'He/She','We', 'You', 'They');
+$lines = 1;
+
+$startup = ["Homelander", "Billy", "Travis", "Arthur"];
+
+$countries = ["UK" => "United Kingdom", 
+"SCT" => "Scotland",
+"WLS" => "Wales", 
+"BE" => "Belgium", 
+"PL" => "Poland", 
+"FR" => "France", 
+"ES" => "Spain",
+"IE" => "Ireland", 
+"JP" => "Japan",
+"us" => "United States"]; 
 
 
 ?>
@@ -16,4 +30,35 @@ $pronouns = array ('I', 'You', 'He/She','We', 'You', 'They');
     }
 
     ?>
+
+    <?php  
+
+while($lines <= 120)
+     {echo $lines;
+     $lines ++;  } 
+
+for($lines = 0; $lines <= 120; $lines ++ ){
+    echo "$lines <br/>"; 
+}
+
+foreach($startup as $values){
+    echo "$values <br/>"; 
+}
+
+    ?>
+
 </pre>
+
+<html>
+    <body>
+        <select>
+            <?php 
+            
+            foreach($countries as $iso => $valuesCountry){
+                echo " <option value = $iso> $valuesCountry </option>"; 
+            }
+            
+            ?>
+        </select>
+    </body>
+</html>
